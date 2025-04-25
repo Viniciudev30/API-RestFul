daeeffa50e661be8746a4607f2d7c5224b7879de
Viniciudev30 <viniciusfaria12@outlook.com>
2025-04-25 16:14:24 -0300

    Conseguindo Passar os dados do médicos,porém ainda sem validação

--- api/src/main/java/med/voli/api/controller/MedicoController.java
+++ api/src/main/java/med/voli/api/controller/MedicoController.java
@@ -2,6 +2,10 @@ package med.voli.api.controller;
 
 
 import med.voli.api.medico.DadosCadastroMedico;
+import med.voli.api.medico.Medico;
+import med.voli.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping ("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
